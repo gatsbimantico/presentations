e7b36e8114597bd33e084803a5dcd01ab60d7877
Cesar Costas <gatsbimantico@gmail.com>
2017-11-07 14:01:06 +0000

    Add posibility of loading a script

---
+++ slides/pwa.js
@@ -0,0 +1,54 @@
+
+    window.notify = function () {
+
+        alert('notify');
+
+    };
+
+    window.geolocation = function () {
+
+        alert('geolocation');
+
+    };
+
+    window.orientation = function () {
+
+        alert('orientation');
+
+    };
+
+    window.video = function () {
+
+        alert('video');
+
+    };
+
+    window.vibration = function () {
+
+        alert('vibration');
+
+    };
+
+    window.ambient = function () {
+
+        alert('ambient');
+
+    };
+
+    window.proximity = function () {
+
+        alert('proximity');
+
+    };
+
+    window.battery = function () {
+
+        alert('battery');
+
+    };
+
+    window.goToGame = function () {
+
+        alert('goToGame');
+
+    };
